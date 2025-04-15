dd1abe5a0d4bb5ef08a9dd1b690db8e607d30191
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-04-15 09:50:31 +0200

    chore: create basic WCF server and client

---
+++ ws/README.md
@@ -0,0 +1,3 @@
+https://github.com/CoreWCF/CoreWCF/blob/main/Documentation/Walkthrough.md
+
+dotnet-svcutil --roll-forward LatestMajor http://localhost:5000/EchoService/basichttp\?wsdl
---
+++ ws/Server/Program.cs
@@ -0,0 +1,29 @@
+using CoreWCF;
+using CoreWCF.Configuration;
+using CoreWCF.Description;
+using CoreWCfDemoServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add WSDL support
+builder.Services.AddServiceModelServices().AddServiceModelMetadata();
+builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+var app = builder.Build();
+
+// Configure an explicit none credential type for WSHttpBinding as it defaults to Windows which requires extra configuration in ASP.NET
+var myWSHttpBinding = new WSHttpBinding(SecurityMode.None);
+
+app.UseServiceModel(builder =>
+{
+    builder.AddService<EchoService>((serviceOptions) => { })
+    // Add a BasicHttpBinding at a specific endpoint
+    .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(), "/EchoService/basichttp")
+    // Add a WSHttpBinding with Transport Security for TLS
+    .AddServiceEndpoint<EchoService, IEchoService>(myWSHttpBinding, "/EchoService/WSHttp");
+});
+
+var serviceMetadataBehavior = app.Services.GetRequiredService<CoreWCF.Description.ServiceMetadataBehavior>();
+serviceMetadataBehavior.HttpGetEnabled = true;
+
+app.Run();
